4912a0edae54446e7f148bbda67685c44c977ba5
Cristian Maya G <cmaya@cesde.net>
2024-02-06 10:13:45 -0500

    Horas extras y ordinarias

--- python/EmploysValidate.py
+++ python/EmploysValidate.py
@@ -2,6 +2,14 @@
 def nomina():
     name = str(input("Digite su nombre: "))
     income = float(input("Digite su salario: "))
+    quantityhour = float(input("Digite la cantidad de horas trabajadas: "))
+    hourvalue = income/240
+    hextras = float(input("Digite el número de horas extras: "))
+
+    valuesalary = hourvalue * quantityhour
+    vextras = hextras * hourvalue * 1.35
+
+
 
 
 
