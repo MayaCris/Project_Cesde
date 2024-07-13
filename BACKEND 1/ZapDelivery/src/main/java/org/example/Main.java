bac17b8f1af993e95bbd817769f6feb7e6d76328
Cristian Maya G <cmaya@cesde.net>
2024-07-13 11:48:44 -0500

    add calls for each validation in main

--- BACKEND 1/ZapDelivery/src/main/java/org/example/Main.java
+++ BACKEND 1/ZapDelivery/src/main/java/org/example/Main.java
@@ -1,33 +1,84 @@
 package org.example;
 
-import java.time.LocalDate;
 import java.util.Scanner;
 
 import org.example.modelos.FechasJava;
+import org.example.modelos.Mercancia;
 import org.example.modelos.Vehiculo;
 
 public class Main {
     public static void main(String[] args) {
 
         Vehiculo vehiculo1 = new Vehiculo();
-//
+
         Scanner sc = new Scanner(System.in);
-////        System.out.println("Ingrese la marca del vehiculo: ");
+//        System.out.println("Ingrese la marca del vehiculo: ");
 //        System.out.println("Ingrese el kilometraje del vehiculo: ");
 //
-////        vehiculo1.setMarca(sc.nextLine());
+//        vehiculo1.setMarca(sc.nextLine());
 //        vehiculo1.setKilometraje(sc.nextDouble());
-
+//
 //        FechasJava fechasJava = new FechasJava();
 //        fechasJava.probarFechas();
+//
+//        System.out.println("Ingrese el ID del vehiculo (Solo números positivos):");
+//        vehiculo1.setId(sc.nextInt());
+//
+//        System.out.println("Ingrese la marca del vehiculo (Solo letras y espacios y máximo 50 caracteres sin caracteres especiales):");
+//        vehiculo1.setMarca(sc.nextLine());
+//
+//        System.out.println("Ingrese el modelo del vehiculo en formato MM/YY: ");
+//        vehiculo1.setModelo(sc.nextLine());
+//
+//        System.out.println("Ingrese el kilometraje del vehiculo (Solo números positivos y máximo 100.000): ");
+//        vehiculo1.setKilometraje(sc.nextDouble());
+//
+//        System.out.println("Ingrese el color del vehiculo (Solo letras y espacios y máximo 20 caracteres): ");
+//        vehiculo1.setColor(sc.nextLine());
+//
+//        System.out.println("Ingrese la descripción del vehiculo (Máximo 100 caracteres): ");
+//        vehiculo1.setDescripcion(sc.nextLine());
+//
+//        System.out.println("Ingrese el tipo del vehiculo (Solo letras sin espacios): ");
+//        vehiculo1.setTipo(sc.nextLine());
+//
+//        System.out.println("Ingrese la autonomía del vehiculo (Solo números positivos y máximo 72): ");
+//        vehiculo1.setAutonomia(sc.nextDouble());
+//
+//        System.out.println("Ingrese la capacidad de carga del vehiculo (Solo números positivos y máximo 1000): ");
+//        vehiculo1.setCapacidadCarga(sc.nextDouble());
+//
+//        System.out.println("Ingrese el avalúo del vehiculo (Solo números positivos): ");
+//        vehiculo1.setAvaluo(sc.nextInt());
 
-        System.out.println("Ingrese el modelo del vehiculo en formato MM-YY: ");
-        vehiculo1.setModelo(sc.nextLine());
+
+        Mercancia mercancia1 = new Mercancia();
+//        System.out.println("Ingrese el ID de la mercancia (Solo números positivos):");
+//        mercancia1.setId(sc.nextInt());
+//
+//        System.out.println("Ingrese el volumen de la mercancia (Solo números positivos y máximo 100): ");
+//        mercancia1.setVolumen(sc.nextDouble());
+//
+//        System.out.println("Ingrese el tipo de la mercancia (Solo letras sin espacios): ");
+//        mercancia1.setTipo(sc.nextLine());
+//
+//        System.out.println("Ingrese el peso de la mercancia (Solo números positivos y máximo 1000): ");
+//        mercancia1.setPeso(sc.nextDouble());
+//
+//        System.out.println("Ingrese la descripción de la mercancia (Máximo 100 caracteres): ");
+//        mercancia1.setDescripcion(sc.nextLine());
+//
+//        System.out.println("Ingrese el avalúo de la mercancia (Solo números positivos): ");
+//        mercancia1.setAvaluo(sc.nextInt());
+//
+//        System.out.println("Ingrese la dirección del remitente de la mercancia (Máximo 200 caracteres): ");
+//        mercancia1.setDireccionRemitente(sc.nextLine());
+//
+//        System.out.println("Ingrese la dirección del destinatario de la mercancia (Máximo 200 caracteres): ");
+//        mercancia1.setDireccionDestinatario(sc.nextLine());
 
     }
 }
 
 
-//Completar validaciones y crear una función que arroje un booleano si la validación es correcta o no => ok
-//Crear enums para las expresiones regulares => ok
 
